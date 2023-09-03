ee4159b66fab967c11e74e72d443f5d164b0b951
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-09-03 12:25:42 -0600

    added string concatination for Microsoft Learn project .NET 7

--- c-sharp-string-concat/Program.cs
+++ c-sharp-string-concat/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+string englishOutput = 
+@"View English output:
+  c:\Exercise\" + projectName + "\\data.txt\n";
+
+string russianOutput = $"{russianMessage}:\n";
+russianOutput = russianOutput + @"  c:\Exercise\" + projectName + @"ru-RU\data.txt";
+
+Console.WriteLine(englishOutput);
+Console.WriteLine(russianOutput);
